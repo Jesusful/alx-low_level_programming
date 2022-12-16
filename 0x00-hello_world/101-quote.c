b8a58f61c9e2a15fc204e2594ce727dbd7c6591c
Jesusful <victoryamusaatlast@gmail.com>
2022-12-16 07:13:34 -0800

    Wow wonderful

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
+#include <unistd.h>
 /**
- * main-Entry
- * Return:Always zero
+ * main - -entry point
+ * Return: 1
  */
 int main(void)
 {
-	fprintf(stderr,"%s","and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(1, "and that piece of art is usefu\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
