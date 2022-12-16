4db112136ba6d28c56e2d8c63a0ef8ca9af34614
Jesusful <victoryamusaatlast@gmail.com>
2022-12-16 05:28:21 -0800

    Error successfully created

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main-Entry
+ * Return:Always zero
+ */
+int main(void)
+{
+	fprintf(stderr,"%s","and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
