4cd75bb6513307546b70d49ff2dbd7faaaf686eb
Jesusful <victoryamusaatlast@gmail.com>
2022-12-20 04:58:29 -0800

    Integers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <limits.h>
+#include <math.h>
+/**
+ * main - Etry point
+ * Return: 0
+ */
+int main(void)
+{
+	int k = 0;
+
+	while (k < 10)
+	{
+		printf("%d\n", k);
+		k++;
+	}
+
+	return (0);
+}
