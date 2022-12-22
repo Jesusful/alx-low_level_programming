663d544c74b1363efa2130738bc3432e78387365
Jesusful <victoryamusaatlast@gmail.com>
2022-12-21 18:36:43 -0800

    'wow wondeful

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/*
+ * void print_alphabet(void): alphabet printing
+ */
+void print_alphabet(void)
+{
+	char l = 'a';
+
+	while (l <= 'z')
+	{
+		_putchar(l);
+		_putchar('\n');
+		l++;
+	}
+}
