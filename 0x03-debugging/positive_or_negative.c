c8d71cfb6d5c7d2b44a28be5937e6c6afe030fb7
Jesusful <victoryamusaatlast@gmail.com>
2022-12-30 08:13:06 -0800

    wow! debugging is fun

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib>
+
+/**
+ * i: integers
+ * positive_or_negative - signed integers
+ *
+ * Return: 0
+ */
+int positive_or_negative(int i)
+{
+	printf("%d\n", i);
+
+	return (0);
+}
