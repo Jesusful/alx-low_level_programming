41ec4f08546c5ccd8d534c8f5bec577c88bc847e
Jesusful <victoryamusaatlast@gmail.com>
2022-12-30 07:38:28 -0800

    Debugging one

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+inlclude "main.h"
+
+/**
+ * main - tests function that prints if intergers is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_ngative(i);
+
+	return (0);
+}
