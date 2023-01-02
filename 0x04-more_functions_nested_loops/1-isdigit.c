b1fb4957da976ab1d60b51e9f56872a93e90bc91
Jesusful <victoryamusaatlast@gmail.com>
2023-01-02 13:49:21 -0800

    Printing digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - Print alpha
+ * @c: digit
+ *
+ * Return: 1 if digit,0 if otherwise
+ */
+int _isdigit(int c)
+{
+	int d = 0;
+
+	if (c >= d && c <= 9)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
