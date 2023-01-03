2350a40120b1ec73907323b4c83ea1cd9c17a496
Jesusful <victoryamusaatlast@gmail.com>
2023-01-03 13:46:37 -0800

    printing FizzBUzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,23 +12,23 @@ int main(void)
 
 	for (x = 1; x <= 100; x++)
 	{
-		if (x % 3 == 0)
+		if ((x % 3 == 0) && (x % 5 == 0))
 		{
-			printf("Fizz");
+			printf("FizzBuzz");
 		}
 		else if (x % 5 == 0)
 		{
 			printf("Buzz");
 		}
-		else if ((x % 5 == 0) && (x % 3 == 0))
+		else if (x % 3 == 0)
 		{
-			printf("FizzBuzz");
+			printf("Fizz");
 		}
 		else
 		{
 			printf("%d", x);
 		}
-		if (x != 100)
+		if (x != 50)
 		{
 			printf(" ");
 		}
