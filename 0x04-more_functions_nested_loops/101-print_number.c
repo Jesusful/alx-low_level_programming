f8d693632768a6b1ab8e0dc8963a3a17829cb33d
Jesusful <victoryamusaatlast@gmail.com>
2023-01-10 17:09:50 -0800

    Priniting Integers

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_number - Numbers printing
+ * @n: integers
+ */
+void print_number(int n)
+{
+	unsigned int a = n;
+
+	if (n < 0)
+	{
+		n *= -1;
+		a = n;
+		_putchar('-');
+	}
+	a /= 10;
+	if (a != 0)
+		print_number(a);
+	_putchar((unsigned int) n % 10 + '0');
+}
