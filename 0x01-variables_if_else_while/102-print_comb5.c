6c6e130217436d6e1b2f1bea76b0c5f8c4c0c412
Jesusful <victoryamusaatlast@gmail.com>
2023-01-11 06:46:00 -0800

    wow again! Two once of integers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - Execution Begins
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j;
+
+	 for (i = 0; i <= ; i++)
+	 {
+		 for (j =
