d3ca2d6cb32ac4fcbcfe9a732424202ac272f4a5
Jesusful <victoryamusaatlast@gmail.com>
2023-01-13 11:04:25 -0800

    Pritning fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Execution
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+	long k = 1, m = 2;
+
+	for (; i < 50; i++)
+	{
+		if (i == 0)
+			printf("%ld", k);
+		else if (i == 1)
+			printf(" ,%ld", m);
+		else
+		{
+			m += k;
+			k = m - m;
+			printf(" ,%ld", m);
+		}
+	}
+	printf("\n");
+	return (0);
+}
